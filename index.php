9b4ef2e8ff015ce8a0554f1c6698905b65440de0
Marcel Soares <marcel_medeiros@ymail.com>
2021-01-19 22:00:34 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -7,10 +7,29 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 //echo json_encode($usuarios);
+//commit: tras um só usuário
+//$root = new Usuario();
+//$root->loadbyID(3);
 
-$root = new Usuario();
-$root->loadbyID(3);
+//echo $root;
 
-echo $root;
+//commit: Carrega uma lista de usuário
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//commit: carrega uma lista de usuario buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//carrega um usúario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+
+echo $usuario;
 
 ?>
